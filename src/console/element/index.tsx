a75bc4407d423a046fd55ad4ddc700d3470db546
lixinxiang <lixinxiang@xiaoyouzi.com>
2018-11-16 10:26:50 +0800

    Element Done

--- src/console/element/index.tsx
+++ src/console/element/index.tsx
@@ -1,20 +1,28 @@
-import * as React from "react";
+import * as React from 'react';
 import { Flex } from 'antd-mobile';
+import { HTMLTree } from '../../components/htmlView/htmlTree';
 
-const FlexItem = Flex.Item
+const FlexItem = Flex.Item;
 
-export class Element extends React.Component<any, any>{
+export class Element extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
   }
   render() {
     return (
       <Flex direction="column" align="stretch" style={{ height: '100%' }}>
-        <FlexItem>
-          html
+        <FlexItem style={{ overflow: 'hidden' }}>
+          <div style={{ padding: '10px', overflow: 'auto', height: '100%' }}>
+            <HTMLTree
+              source={document.documentElement}
+              defaultExpandedTags={['html', 'body']}
+            />
+          </div>
         </FlexItem>
         <Flex align="stretch" style={{ height: '50px', background: '#efefef' }}>
-          <Flex align="center" style={{ flex: 1 }} justify="center">Hide</Flex>
+          <Flex align="center" style={{ flex: 1 }} justify="center">
+            Hide
+          </Flex>
         </Flex>
       </Flex>
     );
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import 'antd-mobile/dist/antd-mobile.css';
-import { HTMLTree } from "./components/htmlView/htmlTree";
+import { MyConsole } from './console';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<HTMLTree source={document.documentElement} defaultExpandedTags={['html', 'body']} />, document.getElementById("root"));
+ReactDOM.render(<MyConsole />, document.getElementById('root'));
 
 registerServiceWorker();
