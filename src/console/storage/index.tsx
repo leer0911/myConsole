23c961c9e7cf03cbd768ed3a97ad95ba19234705
le <445220716@qq.com>
2018-11-17 18:54:56 +0800

    Add  get storage method

--- src/console/storage/index.tsx
+++ src/console/storage/index.tsx
@@ -1,17 +1,12 @@
 import * as React from "react";
-import { Flex, ActionSheet } from 'antd-mobile';
+import { Flex } from 'antd-mobile';
 import Table from 'rc-table';
 
 const columns = [{
-  title: 'Name', dataIndex: 'name', key: 'name', width: 100,
+  title: 'Name', dataIndex: 'key', key: 'key'
 }, {
-  title: 'Value', dataIndex: 'value', key: 'value', width: 100,
+  title: 'Value', dataIndex: 'value', key: 'value'
 }];
-
-const data = [
-  { name: 'Jack', value: 28 },
-  { name: 'Rose', value: 36 },
-];
 
 const FlexItem = Flex.Item
 
@@ -19,17 +14,65 @@ export class Storage extends React.Component<any, any>{
   constructor(props: any) {
     super(props);
   }
-  showFilter() {
-    const BUTTONS = ['All', 'Log', 'Info', 'Warn', 'Error'];
-    ActionSheet.showActionSheetWithOptions({
-      options: BUTTONS
+  getCookieList() {
+    if (!document.cookie || !navigator.cookieEnabled) {
+      return [];
+    }
+
+    const list: any[] = [];
+    const items = document.cookie.split(';');
+
+    items.forEach((item: any) => {
+      const ret = item.split('=')
+      if (ret) {
+        const key = ret.shift().replace(/^ /, '');
+        const value = ret.join('=');
+        list.push({
+          key: decodeURIComponent(key),
+          value: decodeURIComponent(value)
+        })
+      }
     })
+
+    return list;
   }
+
+  getLocalStorageList() {
+    if (!window.localStorage) {
+      return [];
+    }
+
+    try {
+      const list: any[] = []
+      for (const key in localStorage) {
+        if (localStorage.hasOwnProperty(key)) {
+          const value = localStorage[key];
+          list.push({
+            key,
+            value
+          });
+        }
+      }
+      return list;
+    } catch (e) {
+      return [];
+    }
+  }
+
   render() {
+    const data = [
+      {
+        key: 'cookie', value: '', children: this.getCookieList()
+      },
+      {
+        key: 'localStorage', value: '', children: this.getLocalStorageList()
+      },
+    ];
+
     return (
       <Flex direction="column" align="stretch" style={{ height: '100%' }}>
         <FlexItem>
-          <Table columns={columns} data={data} />
+          <Table expandRowByClick expandIconAsCell useFixedHeader columns={columns} data={data} />
         </FlexItem>
         <Flex align="stretch" style={{ height: '50px', background: '#efefef' }}>
           <Flex align="center" style={{ flex: 1, borderRight: '1px solid #ddd' }} justify="center">Refresh</Flex>
